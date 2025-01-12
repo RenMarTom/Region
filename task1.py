0f63c19367f1c031f95e47ab0c6c814ff5a05bb9
RenMarTom <105501877+RenMarTom@users.noreply.github.com>
2025-01-12 13:12:03 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 # initialisation
+import time
 n, m = input().split()  # n - rows number, m - pax number
 seats_beg = []
 n = int(n)
@@ -14,6 +15,9 @@ for i in range(n):
     seats_beg.append(list(input()))
 
 
+start = time.time()
+
+
 def get_inform():
     global seats_beg, left_x, right_x, left_seats, right_seats, n, x_beg
     for i in range(n):
@@ -25,23 +29,27 @@ def get_inform():
         left_seats.append(seats_beg[i][:3])
         right_seats.append(seats_beg[i][:3])
 
+get_inform()
 
 # impossible situations
 if m > (6 * n - x_beg):
     print('Impossible')
+    print(time.time() - start)
     check = True
-if (x_beg % 2 != m % 2) and check == False:
-    print('Impossible')
-    check = True
+# if (x_beg % 2 != m % 2) and check == False:
+#     print('Impossible')
+#     print(time.time() - start)
+#     check = True
 
 # task 1
 if m == 0 and check == False:
-    get_inform()
     if left_seats == right_seats:
         for i in range(n):
             print(''.join(seats_beg[i]))
+        print(time.time() - start)
     else:
         print('Impossible')
+        print(time.time() - start)
         check = True
 
 # task 2
@@ -72,16 +80,18 @@ if x_beg == 0 and check == False:
         if check == False:
             for i in range(n):
                 print(''.join(seats_beg[i]))
+            print(time.time() - start)
         else:
             print('Impossible')
+            print(time.time() - start)
             check = True
     else:
         print('Impossible')
+        print(time.time() - start)
         check = True
 
 # task 3
 if m == 1 and check == False:
-    get_inform()
     if abs(left_x - right_x) == 1 and x_beg != n * 6:
         i = 0
         while check != True or i < n:
@@ -96,10 +106,13 @@ if m == 1 and check == False:
             x_beg[ch_val1][ch_val2] = 'X'
             for i in range(n):
                 print(''.join(seats_beg[i]))
+            print(time.time() - start)
         else:
             print('Impossible')
+            print(time.time() - start)
     else:
         print('Impossible')
+        print(time.time() - start)
 
 # taask 4
 if x_beg == 1 and check == False:
@@ -137,7 +150,14 @@ if x_beg == 1 and check == False:
                     seats_beg[r][5] = 'X'
                     m -= 2
                     r += 1
+            if check == True:
+                x_beg[ch_val1][ch_val2] = 'X'
+                for i in range(n):
+                    print(''.join(seats_beg[i]))
+                print(time.time() - start)
         else:
             print('Impossible')
+            print(time.time() - start)
     else:
         print('Impossible')
+        print(time.time() - start)
